34216fd43f5424eca455d8483f97c4ee638e9655
100rabhg <sourabhpatware100@gmail.com>
2024-01-12 13:42:10 +0530

    server path fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,7 +63,7 @@ namespace :deploy do
   task :restart do
     on roles(:app) do
       within release_path do
-        execute "ruby $HOME/StockManage/start_server.rb #{ fetch(:rails_env) } #{ '-d' }"
+        execute "ruby $HOME/StockManage/current/start_server.rb #{ fetch(:rails_env) }"
       end
     end
   end
--- start_server.rb
+++ start_server.rb
@@ -1,11 +1,12 @@
 stdout_str = %x( lsof -wni tcp:3000 )
 unless stdout_str.empty?
     stdout_str = system( "kill -INT #{stdout_str.lines[1].split(' ')[1]}" )
-    puts 'Server Stoped'
+    puts 'Server Stoped' if stdout_str
 end
-puts %x( pwd )
+
 unless ARGV.empty?
-    puts %x( rails s -e #{ ARGV[0] } #{ (ARGV.count > 1) ? ARGV[1] : '' }  )
+    system( 'cd $HOME/StockManage/current' )
+    puts %x( rails s -e #{ ARGV[0] } -d )
 else
     puts 'running environment missing'
 end
